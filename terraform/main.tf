7410a8ef171e084ddb6cd286b6057a1f4ea6a312
maheshmani789 <maheshmani789@gmail.com>
2025-09-25 16:22:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,19 +3,45 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# Define an EC2 instance
+# Add a variable for the Jenkins server's IP
+variable "jenkins_server_ip" {
+  type        = string
+  description = "The public IP of the Jenkins server"
+}
+
+# Create a security group for the web server
+resource "aws_security_group" "web_server_sg" {
+  name        = "web_server_sg"
+  description = "Allow inbound traffic for web access and SSH from Jenkins"
+
+  # Rule to allow HTTP traffic from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Rule to allow SSH traffic from the Jenkins server only
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.jenkins_server_ip}/32"]
+  }
+}
+
+# Define an EC2 instance and attach the security group
 resource "aws_instance" "web_server" {
-  # !!! IMPORTANT: Replace this with a valid Ubuntu AMI ID for your region !!!
-  # You can find one in the AWS EC2 launch wizard.
   ami           = "ami-07f07a6e1060cd2a8"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_server_sg.id]
   tags = {
     Name = "MyWebServer"
   }
 }
 
-# Output the public IP address of the server
-output "server_ip" {
+# Output the public IP address of the web server
+output "web_server_ip" {
   value = aws_instance.web_server.public_ip
-
 }
